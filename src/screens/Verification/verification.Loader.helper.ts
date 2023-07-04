2be84e1cfc968e47303c97196c48d3a66e711734
Ibraheem Saoud <saoudibraheem@gmail.com>
2023-07-04 19:51:19 +0200

    fix verification process

--- src/screens/Verification/verification.Loader.helper.ts
+++ src/screens/Verification/verification.Loader.helper.ts
@@ -1,13 +1,21 @@
-import { Navigate, Params } from "react-router-dom";
 import { toast } from "react-hot-toast";
 import { updateVerification } from "../../requests";
 import { HOME } from "../../consts/links.const";
+import { useUser } from "../../hooks";
 
-//https://archivals.eu/verification?userId=64a44814b385b8ea84a5&secret=a74f9d514f6d641eaba88cfdc0604b3e6c93316ab54eb80d03a3ac9dc3175be37bcd22d960074facf52807536e9e50382c355326c1666dd34d6bcb07f6c078615947a8ba15a77418fdc816cf570b056b3f021fe45bb42a7432b102d5ab43264b68d8bb40a357fec85cd4634d11d61c4cc3332aad35c8bddae84f7c3639593df7&expire=2023-07-11+16%3A40%3A06.680
 export const verificationLoader =
   () =>
-  async ({ params }: { params: Params<string> }) => {
-    const { userId, secret, expire } = params;
+  async ({ request }: { request: Request }) => {
+    const { user } = useUser();
+    if (user?.emailVerification) {
+      window.location.href = HOME;
+      return {};
+    }
+    const url = new URL(request.url);
+    const userId = url.searchParams.get("userId");
+    const secret = url.searchParams.get("secret");
+    const expire = url.searchParams.get("expire");
+
     if (!userId || !secret || !expire) {
       toast.error(
         "failed to verify, please click the link again or contact support",
@@ -21,6 +29,7 @@ export const verificationLoader =
       );
       return {};
     }
+
     try {
       const { error } = await updateVerification(userId, secret);
       if (error) {
@@ -36,7 +45,7 @@ export const verificationLoader =
         );
         return {};
       }
-      Navigate({ to: HOME, replace: true });
+      window.location.href = HOME;
     } catch (e) {
       toast.error(e as string, {
         style: {
