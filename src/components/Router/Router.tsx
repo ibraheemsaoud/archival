6abfc208d973feaca5d0694ee6a64d232135a135
Ibraheem Saoud <saoudibraheem@gmail.com>
2023-07-04 19:31:12 +0200

    fixed verification loader

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,11 +1,19 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { ENTRY, ERA, HOME, PROFILE, TOPIC, VERIFICATION } from "../../consts/links.const";
+import {
+  ENTRY,
+  ERA,
+  HOME,
+  PROFILE,
+  TOPIC,
+  VERIFICATION,
+} from "../../consts/links.const";
 import { TopicList, topicListLoader } from "../../screens/TopicList";
 import { EraList, eraListLoader } from "../../screens/EraList";
 import { Era, eraLoader } from "../../screens/Era";
 import { Entry, entryLoader } from "../../screens/Entry";
 import { Profile } from "../../screens/Profile";
 import { Verification } from "../../screens/Verification";
+import { verificationLoader } from "../../screens/Verification/verification.Loader.helper";
 
 export const Router = () => {
   const router = createBrowserRouter([
@@ -33,10 +41,12 @@ export const Router = () => {
       path: PROFILE,
       element: <Profile />,
       loader: topicListLoader(),
-    }, {
+    },
+    {
       path: VERIFICATION,
       element: <Verification />,
-    }
+      loader: verificationLoader(),
+    },
   ]);
 
   return <RouterProvider router={router} />;
