d0e44bfa23cda774bf788a87e020435407686ea2
Ibraheem Saoud <saoudibraheem@gmail.com>
2023-08-12 13:46:49 +0200

    small fix

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -17,6 +17,7 @@ export const Home = () => {
           display: "flex",
           padding: 1,
           borderBottom: "1px solid #e0e0e0",
+          overflowX: "auto",
         }}
       >
         {brands?.map((brand) => (
