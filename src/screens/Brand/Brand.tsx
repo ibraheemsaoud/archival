1f34c770181ed165acf9f7405f32ed680c6a177d
Ibraheem Saoud <saoudibraheem@gmail.com>
2023-08-17 19:57:07 +0200

    added latest season feature to brands

--- src/screens/Brand/Brand.tsx
+++ src/screens/Brand/Brand.tsx
@@ -25,6 +25,8 @@ export const Brand = () => {
   if (!seasons || !brand) return <div>Loading...</div>;
 
   const modedTheme = theme("light", brand.primaryColor, brand.secondaryColor);
+  const lastSeason = seasons[0];
+  const restOfSeasons = seasons.slice(1);
 
   return (
     <ThemeProvider theme={modedTheme}>
@@ -56,11 +58,10 @@ export const Brand = () => {
                   {brand.name}
                 </Typography>
               </Box>
-
               <Button
                 size="small"
                 variant="outlined"
-                sx={{ marginBottom: 1 }}
+                sx={{ marginBottom: 1, visibility: "hidden" }}
                 onClick={() => {}}
               >
                 Chat
@@ -68,18 +69,36 @@ export const Brand = () => {
             </Toolbar>
           </AppBar>
         </Box>
-        <Box marginX={1} marginY={2}>
-          <Typography variant="h6" component="div" sx={{ textDecoration: "underline" }}>
-            Seasons
-          </Typography>
-          <Grid container spacing={1}>
-            {seasons.map((season: ISeason) => (
-              <Grid item xs={6} lg={4} key={season.$id}>
-                <SeasonCard seasonId={season.slug} brandView />
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
+        {lastSeason && (
+          <>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ textDecoration: "underline" }}
+            >
+              Latest Season
+            </Typography>
+            <SeasonCard seasonId={lastSeason.slug} brandView />
+          </>
+        )}
+        {restOfSeasons.length > 0 && (
+          <Box marginX={1} marginY={2}>
+            <Typography
+              variant="h6"
+              component="div"
+              sx={{ textDecoration: "underline" }}
+            >
+              Seasons
+            </Typography>
+            <Grid container spacing={1}>
+              {restOfSeasons.map((season: ISeason) => (
+                <Grid item xs={6} lg={4} key={season.$id}>
+                  <SeasonCard seasonId={season.slug} brandView />
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        )}
       </AppWrapper>
     </ThemeProvider>
   );
