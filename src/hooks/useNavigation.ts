68f3db4204b4a81e21f3a1918ab29fff62178718
Ibraheem Saoud <saoudibraheem@gmail.com>
2023-12-09 10:37:23 +0100

    fixed the back 🔙 navigation

--- src/hooks/useNavigation.ts
+++ src/hooks/useNavigation.ts
@@ -3,11 +3,11 @@ import { useNavigate } from "react-router-dom";
 export const useNavigation = () => {
   const navigate = useNavigate();
 
-  const onBack = (defaultRoute?: string) => {
-    if (defaultRoute) {
-      navigate(defaultRoute);
-    } else {
+  const onBack = (defaultRoute: string) => {
+    if (window.history?.length) {
       navigate(-1);
+    } else {
+      navigate(defaultRoute);
     }
   };
 
